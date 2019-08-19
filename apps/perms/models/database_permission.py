96ffacd560bb41af6f3e1b86b05c10f6b35f2323
BaiJiangJie <bugatti_it@163.com>
2019-08-19 14:21:42 +0800

    [Feature] Permission: 添加 DatabasePermission Model

---
+++ apps/perms/models/database_permission.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+#
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .base import BasePermission
+
+
+__all__ = ['DatabasePermission']
+
+
+class DatabasePermission(BasePermission):
+    databases = models.ManyToManyField(
+        'applications.Database', related_name='granted_by_permissions',
+        blank=True, verbose_name=_("Database")
+    )
+
+    class Meta:
+        unique_together = [('org_id', 'name')]
+        verbose_name = _('Database permission')
+        ordering = ('name',)

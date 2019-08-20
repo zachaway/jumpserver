a3a9335a8886a1262e64cb987bc7d4bc08a95602
BaiJiangJie <bugatti_it@163.com>
2019-08-20 15:17:35 +0800

    [Feature] Permission: 添加 DatabasePermission API (用户组授权)

---
+++ apps/perms/api/user_group_database_permission.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+#
+
+
+from rest_framework.generics import ListAPIView, get_object_or_404
+
+from common.permissions import IsOrgAdminOrAppUser
+from ..utils import DatabasePermissionUtil
+from ..hands import UserGroup, DatabaseSerializer
+
+__all__ = ['UserGroupGrantedDatabasesApi']
+
+
+class UserGroupGrantedDatabasesApi(ListAPIView):
+    permission_classes = (IsOrgAdminOrAppUser,)
+    serializer_class = DatabaseSerializer
+
+    def get_queryset(self):
+        queryset = []
+        user_group_id = self.kwargs.get('pk')
+        if not user_group_id:
+            return queryset
+        user_group = get_object_or_404(UserGroup, id=user_group_id)
+        util = DatabasePermissionUtil(user_group)
+        queryset = util.get_databases()
+        return queryset
--- apps/perms/urls/api_urls.py
+++ apps/perms/urls/api_urls.py
@@ -93,6 +93,8 @@ database_permission_urlpatterns = [
     path('users/<uuid:pk>/databases/tree/', api.UserGrantedDatabasesAsTreeApi.as_view(), name='user-databases-as-tree'),
     path('users/databases/tree/', api.UserGrantedDatabasesAsTreeApi.as_view(), name='my-databases-as-tree'),
 
+    path('user-groups/<uuid:pk>/databases/', api.UserGroupGrantedDatabasesApi.as_view(), name='user-group-databases'),
+
     path('database-permissions/<uuid:pk>/user/add/', api.DatabasePermissionAddUserApi.as_view(), name='database-permission-add-user'),
     path('database-permissions/<uuid:pk>/user/remove/', api.DatabasePermissionRemoveUserApi.as_view(), name='database-permission-remove-user'),
     path('database-permissions/<uuid:pk>/database/add/', api.DatabasePermissionAddDatabaseApi.as_view(), name='database-permission-add-database'),
